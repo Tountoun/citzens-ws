c1938935fef7303061d68144202598de17cf3d5e
Tountoun <tountounabela@gmail.com>
2023-11-04 01:16:42 +0000

    Init project

---
+++ src/main/java/com/gofar/citzensws/CitzensWsApplication.java
@@ -0,0 +1,13 @@
+package com.gofar.citzensws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitzensWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitzensWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gofar/citzensws/CitzensWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gofar.citzensws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitzensWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
