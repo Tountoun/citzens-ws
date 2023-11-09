e75472dbc09fedbb3fed030eb8ba5f442cfd34d0
Tountoun <tountounabela@gmail.com>
2023-11-09 17:42:47 +0000

    Retrieving citizen's data from a database

--- src/main/java/com/gofar/citzensws/repository/CitizenRepository.java
+++ src/main/java/com/gofar/citzensws/repository/CitizenRepository.java
@@ -1,6 +1,15 @@
-package com.gofar.citzensws.controller;
+package com.gofar.citzensws.repository;
 
+import com.gofar.citzensws.entity.Citizen;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CitizenRepository extends JpaRepository {
+import java.util.Optional;
+
+@Repository
+public interface CitizenRepository extends JpaRepository<Citizen, Long> {
+
+    @EntityGraph(attributePaths = {"mother", "father", "job"})
+    Optional<Citizen> findByCin(String cin);
 }
--- src/main/java/com/gofar/citzensws/services/CitizenService.java
+++ src/main/java/com/gofar/citzensws/services/CitizenService.java
@@ -1,22 +1,49 @@
 package com.gofar.citzensws.services;
 
-import com.gofar.citzensws.utils.Data;
-import com.gofar.ws.Citizen;
+import com.gofar.citzensws.entity.Citizen;
+import com.gofar.citzensws.repository.CitizenRepository;
+import com.gofar.citzensws.utils.LocalDateXmlConverter;
 import com.gofar.ws.GetCitizenInfoRequest;
 import com.gofar.ws.GetCitizenInfoResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class CitizenService {
 
+    private CitizenRepository citizenRepository;
+
     public GetCitizenInfoResponse getCitizenInfoResponse(GetCitizenInfoRequest request) throws Exception {
         GetCitizenInfoResponse response = new GetCitizenInfoResponse();
 
-        Citizen citizen = Data.citizens().stream()
-                .filter(citizen1 -> citizen1.getCni().equals(request.getCni()))
-                .findFirst().orElse(null);
-        response.setData(citizen);
+        Citizen citizen = citizenRepository.findByCin(request.getCni()).orElseThrow(EntityNotFoundException::new);
+        com.gofar.ws.Citizen wsCitz = new com.gofar.ws.Citizen();
+        wsCitz.setCni(citizen.getCin());
+        wsCitz.setFirstName(citizen.getFirstName());
+        wsCitz.setLastName(citizen.getLastName());
+        wsCitz.setBirthDay(LocalDateXmlConverter.marshal(citizen.getBirthDay()));
+        wsCitz.setHeight(citizen.getHeight());
+        String mother = "";
+        String father = "";
+        if (Objects.nonNull(citizen.getMother())) {
+            mother = citizen.getMother().getLastName() + " " + citizen.getMother().getFirstName();
+        }
+        if (Objects.nonNull(citizen.getFather())) {
+            father = citizen.getFather().getLastName() + " " + citizen.getFather().getFirstName();
+        }
+        wsCitz.setFatherName(father);
+        wsCitz.setMotherName(mother);
+        wsCitz.setJob(citizen.getJob().getName());
+        response.setData(wsCitz);
+
         return response;
     }
 
+    @Autowired
+    public void setCitizenRepository(CitizenRepository citizenRepository) {
+        this.citizenRepository = citizenRepository;
+    }
 }
